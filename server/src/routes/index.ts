9b8f8bd439a09bb9c6cb229b78bf1b7a43c776ba
Ansh Mourya <anshmourya657@gmail.com>
2024-02-25 11:29:31 +0530

    check

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.send('hello wroldssss');
+	res.send('hello wroldsssss');
 });
 
 export default router;
