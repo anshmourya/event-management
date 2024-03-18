70568e71b22a467e704d9cd25c69e00b3f14a9ce
ansh mourya <ansh@levitation.co.in>
2024-03-18 09:07:54 +0530

    feat: event template

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,38 @@
+import { H5, P } from "@/components/typograph";
 import { Button } from "@/components/ui/button";
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 const Home = () => {
   return (
-    <div>
-      <Button>ansh mourya</Button>
+    <div className="container mt-10">
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3">
+        {[1, 2, 3, 4, 5, 6, 6, 6, 6, 6].map((data) => (
+          <Card
+            className="max-w-[300px] capitalize m-3 cursor-pointer"
+            key={data}
+          >
+            <CardHeader>
+              <CardTitle>
+                <H5>Ticket Left </H5>
+              </CardTitle>
+              <CardDescription>i dont know anything</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+              <P className="my-3">only 200 ticket left</P>
+              <img
+                src="https://static-cse.canva.com/blob/1396716/1600w-wlXEWqHuexQ.jpg"
+                alt=" current event"
+              />
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
